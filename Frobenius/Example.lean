75841bef8abad6c6098c51872f8d98533ba7cb2d
igorkhavkine <igor@tichy>
2024-12-28 23:34:13 +0100

    Started adding refs to things already in Mathlib.

--- Frobenius/Example.lean
+++ Frobenius/Example.lean
@@ -1,1 +1,4 @@
-#eval 1 + 2
+import Mathlib.Topology.VectorBundle.Basic
+import Mathlib.Geometry.Manifold.SmoothManifoldWithCorners
+import Mathlib.Geometry.Manifold.VectorBundle.SmoothSection
+import Mathlib.Geometry.Manifold.VectorBundle.Tangent
