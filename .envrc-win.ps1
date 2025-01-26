85129360475349fdc4eae68b533ae273b154dc3e
igorkhavkine <igor@tichy>
2025-01-26 16:31:58 +0100

    Hopefully fixed the custom pwsh environment on Windows.

--- .envrc-win.ps1
+++ .envrc-win.ps1
@@ -1,3 +1,3 @@
 $env:VSCODE_INJECTION = 1  # seems not passed automatically to custom profiles
-& (code --locate-shell-integration-path pwsh)
-.\.env\Scripts\activate
+. (code --locate-shell-integration-path pwsh)
+. .\.env\Scripts\activate.ps1
