aa6ce7e63598cbd607c280b1986d443ed4236c0f
igorkhavkine <igor@tichy>
2025-08-29 09:18:57 +0200

    Added copyright notices to main files.

--- Frobenius/ODE.lean
+++ Frobenius/ODE.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2025 Igor Khavkine. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Igor Khavkine
+-/
 import Mathlib.Analysis.Calculus.MeanValue
 import Mathlib.Analysis.ODE.PicardLindelof
 import Mathlib.Analysis.ODE.Gronwall
